8dc26f96551374531d638de7b37e26b0a41f0157
kherma <kamil.herma01@gmail.com>
2020-11-02 21:36:17 +0100

    Fix eslint errors with settings.js and utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,80 +1,81 @@
+/* eslint-disable no-prototype-builtins */
 /* global Handlebars, dataSource */
 
-export const utils = {};
+export const utils = {}
 
-utils.createDOMFromHTML = function(htmlString) {
-  let div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
-};
+utils.createDOMFromHTML = function (htmlString) {
+  const div = document.createElement('div')
+  div.innerHTML = htmlString.trim()
+  return div.firstChild
+}
 
-utils.createPropIfUndefined = function(obj, key, value = []){
-  if(!obj.hasOwnProperty(key)){
-    obj[key] = value;
+utils.createPropIfUndefined = function (obj, key, value = []) {
+  if (!obj.hasOwnProperty(key)) {
+    obj[key] = value
   }
-};
+}
 
-utils.serializeFormToObject = function(form){
-  let output = {};
-  if (typeof form == 'object' && form.nodeName == 'FORM') {
-    for (let field of form.elements) {
-      if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
-        if (field.type == 'select-multiple') {
-          for (let option of field.options) {
-            if(option.selected) {
-              utils.createPropIfUndefined(output, field.name);
-              output[field.name].push(option.value);
+utils.serializeFormToObject = function (form) {
+  const output = {}
+  if (typeof form === 'object' && form.nodeName === 'FORM') {
+    for (const field of form.elements) {
+      if (field.name && !field.disabled && field.type !== 'file' && field.type !== 'reset' && field.type !== 'submit' && field.type !== 'button') {
+        if (field.type === 'select-multiple') {
+          for (const option of field.options) {
+            if (option.selected) {
+              utils.createPropIfUndefined(output, field.name)
+              output[field.name].push(option.value)
             }
           }
-        } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
-          utils.createPropIfUndefined(output, field.name);
-          output[field.name].push(field.value);
+        } else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
+          utils.createPropIfUndefined(output, field.name)
+          output[field.name].push(field.value)
         }
       }
     }
   }
-  return output;
-};
+  return output
+}
 
-utils.queryParams = function(params){
+utils.queryParams = function (params) {
   return Object.keys(params)
     .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-    .join('&');
-};
+    .join('&')
+}
 
-utils.convertDataSourceToDbJson = function(){
-  const productJson = [];
-  for(let key in dataSource.products){
-    productJson.push(Object.assign({id: key}, dataSource.products[key]));
+utils.convertDataSourceToDbJson = function () {
+  const productJson = []
+  for (const key in dataSource.products) {
+    productJson.push(Object.assign({ id: key }, dataSource.products[key]))
   }
 
-  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
-};
+  console.log(JSON.stringify({ product: productJson, order: [] }, null, '  '))
+}
 
-utils.numberToHour = function(number){
-  return (Math.floor(number) % 24) + ':' + (number % 1 * 60 + '').padStart(2, '0');
-};
+utils.numberToHour = function (number) {
+  return (Math.floor(number) % 24) + ':' + (number % 1 * 60 + '').padStart(2, '0')
+}
 
-utils.hourToNumber = function(hour){
-  const parts = hour.split(':');
+utils.hourToNumber = function (hour) {
+  const parts = hour.split(':')
 
-  return parseInt(parts[0]) + parseInt(parts[1])/60;
-};
+  return parseInt(parts[0]) + parseInt(parts[1]) / 60
+}
 
-utils.dateToStr = function(dateObj){
-  return dateObj.toISOString().slice(0, 10);
-};
+utils.dateToStr = function (dateObj) {
+  return dateObj.toISOString().slice(0, 10)
+}
 
-utils.addDays = function(dateStr, days){
-  const dateObj = new Date(dateStr);
-  dateObj.setDate(dateObj.getDate() + days);
-  return dateObj;
-};
+utils.addDays = function (dateStr, days) {
+  const dateObj = new Date(dateStr)
+  dateObj.setDate(dateObj.getDate() + days)
+  return dateObj
+}
 
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
+Handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  return (arg1 === arg2) ? options.fn(this) : options.inverse(this)
+})
 
-Handlebars.registerHelper('joinValues', function(input, options) {
-  return Object.values(input).join(options.fn(this));
-});
+Handlebars.registerHelper('joinValues', function (input, options) {
+  return Object.values(input).join(options.fn(this))
+})
