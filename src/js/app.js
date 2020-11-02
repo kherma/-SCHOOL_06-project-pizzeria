e3b3d05a17b1c623ee00bb78d2c77b3580a0f9df
kherma <kamil.herma01@gmail.com>
2020-11-02 23:18:15 +0100

    Add subpage switching feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,65 @@
-import { settings, select } from './settings.js'
+import { settings, select, classNames } from './settings.js'
 import Product from './components/Product.js'
 import Cart from './components/Cart.js'
 
 const app = {
+  initPages: function () {
+    const thisApp = this
+    thisApp.pages = document.querySelector(select.containerOf.pages).children
+    thisApp.navLinks = document.querySelectorAll(select.nav.links)
+
+    const idFromHash = window.location.hash.replace('#/', '')
+
+    let pageMatchingHash = thisApp.pages[0].id
+
+    for (const page of thisApp.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id
+        break
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash)
+    // for (const page of thisApp.pages) {
+    //   if (page.id === idFromHash) {
+    //     thisApp.activatePage(idFromHash)
+    //   } else {
+    //     thisApp.activatePage(thisApp.pages[0].id)
+    //   }
+    // }
+
+    for (const link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickElement = this
+        event.preventDefault()
+
+        // Get page if from href
+        const id = clickElement.getAttribute('href').replace('#', '')
+
+        // run thisApp.activatePage with that id
+        thisApp.activatePage(id)
+
+        // Change URL hash
+        window.location.hash = `#/${id}`
+      })
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this
+    // add class "active" to matching pages, remove from non-matching
+    for (const page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id === pageId)
+    }
+    // add class "active" to matching links, remove from non-matching
+    for (const link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') === `#${pageId}`
+      )
+    }
+  },
+
   initMenu: function () {
     const thisApp = this
     // console.log('thisApp.data: ',thisApp.data);
@@ -24,15 +81,12 @@ const app = {
         return rawResponde.json()
       })
       .then(function (parsedResponde) {
-        console.log('parsedResponde', parsedResponde)
-
         // save parsedResponde as thisApp.data.products
         thisApp.data.products = parsedResponde
 
         // execute initMenu method
         thisApp.initMenu()
       })
-    console.log('thisApp.data', JSON.stringify(thisApp.data))
   },
 
   initCart: function () {
@@ -55,6 +109,7 @@ const app = {
     // console.log('classNames:', classNames);
     // console.log('settings:', settings);
     // console.log('templates:', templates);
+    thisApp.initPages()
     thisApp.initData()
     thisApp.initCart()
   }
