04cf119f50bd265a00397a6dc623729b7f02faf9
kherma <kamil.herma01@gmail.com>
2020-11-15 21:25:33 +0100

    Add updateDOM method

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { templates, select, settings } from '../settings.js'
+import { templates, select, settings, classNames } from '../settings.js'
 import { utils } from '../utils.js'
 import AmountWidget from './AmountWidget.js'
 import DatePicker from './DatePicker.js'
@@ -28,7 +28,7 @@ class Booking {
       eventsCurrent: `${settings.db.url}/${settings.db.event}?${params.eventsCurrent.join('&')}`,
       eventsRepeat: `${settings.db.url}/${settings.db.event}?${params.eventsRepeat.join('&')}`
     }
-    console.log(urls)
+
     Promise.all([
       fetch(urls.booking),
       fetch(urls.eventsCurrent),
@@ -45,10 +45,74 @@ class Booking {
         ])
       })
       .then(function ([bookings, eventsCurrent, eventsRepeat]) {
-        console.log(bookings)
-        console.log(eventsCurrent)
-        console.log(eventsRepeat)
+        // console.log(bookings)
+        // console.log(eventsCurrent)
+        // console.log(eventsRepeat)
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat)
       })
+  }
+
+  parseData (bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this
+    thisBooking.booked = {}
+    for (const item of eventsCurrent) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table)
+    }
+
+    const minDate = thisBooking.datePicker.minDate
+    const maxDate = thisBooking.datePicker.maxDate
+    for (const item of eventsRepeat) {
+      if (item.repeat === 'daily') {
+        for (let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)) {
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table)
+        }
+      }
+    }
+
+    for (const item of bookings) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table)
+    }
+    thisBooking.updateDOM()
+  }
+
+  makeBooked (date, hour, duration, table) {
+    const thisBooking = this
+    if (typeof thisBooking.booked[date] === 'undefined') {
+      thisBooking.booked[date] = {}
+    }
+
+    const startHour = utils.hourToNumber(hour)
+
+    for (let i = startHour; i < startHour + duration; i += 0.5) {
+      if (typeof thisBooking.booked[date][i] === 'undefined') {
+        thisBooking.booked[date][i] = []
+      }
+      thisBooking.booked[date][i].push(table)
+    }
+  }
+
+  updateDOM () {
+    const thisBooking = this
+    thisBooking.date = thisBooking.datePicker.value
+    thisBooking.hour = utils.hourToNumber(thisBooking.hourPicker.value)
+
+    let allAvailable = false
+
+    if (typeof thisBooking.booked[thisBooking.date] === 'undefined' || typeof thisBooking.booked[thisBooking.date][thisBooking.hour] === 'undefined') {
+      allAvailable = true
+    }
+
+    for (const table of thisBooking.dom.tables) {
+      const tableId = parseInt(table.getAttribute(settings.booking.tableIdAttribute))
+
+      if (
+        !allAvailable &&
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)) {
+        table.classList.add(classNames.booking.tableBooked)
+      } else {
+        table.classList.remove(classNames.booking.tableBooked)
+      }
+    }
   }
 
   render (element) {
@@ -71,6 +135,7 @@ class Booking {
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount)
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper)
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper)
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(select.booking.tables)
   }
 
   initWidgets () {
@@ -79,6 +144,9 @@ class Booking {
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount)
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker)
     thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker)
+    thisBooking.dom.wrapper.addEventListener('updated', function () {
+      thisBooking.updateDOM()
+    })
   }
 }
 
